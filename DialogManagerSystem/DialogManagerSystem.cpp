e5d91c7f5ba188d6047fdb8ef0a75e26d6324b10
Andrew Logan <9957173@student.toiohomai.ac.nz>
2024-08-01 23:43:08 +1200

    added more dialog

--- DialogManagerSystem/DialogManagerSystem.cpp
+++ DialogManagerSystem/DialogManagerSystem.cpp
@@ -1,18 +1,35 @@
-// DialogManagerSystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "DialogSystem.h"
 
+using namespace std;
 int main()
-{
-    auto root = std::make_shared<DialogNode>("Welcome to the game! What will you do?");
-    auto option1 = std::make_shared<DialogNode>("You chose to fight the dragon!");
-    auto option2 = std::make_shared<DialogNode>("You chose to run away!");
+{    
+    // Create the root dialog node
+    auto root = make_shared<DialogNode>("Welcome to the game! What will you do?");
+
+    // Create first set of options
+    auto fightDragon = make_shared<DialogNode>("You chose to fight the dragon! It's a fierce battle...");
+    auto runAway = make_shared<DialogNode>("You chose to run away! You live to fight another day.");
 
     // Add options to the root node
-    root->AddOption("Fight the dragon", option1);
-    root->AddOption("Run away", option2);
+    root->AddOption("Fight the dragon", fightDragon);
+    root->AddOption("Run away", runAway);
+
+    // Additional dialog nodes for fighting the dragon
+    auto useSword = make_shared<DialogNode>("You use your sword and slay the dragon!");
+    auto useMagic = make_shared<DialogNode>("You use magic to defeat the dragon, but at a cost...");
+
+    // Add more options to the fightDragon node
+    fightDragon->AddOption("Use sword", useSword);
+    fightDragon->AddOption("Use magic", useMagic);
+
+    // Additional dialog nodes for running away
+    auto hideInCave = make_shared<DialogNode>("You hide in a cave and wait for the danger to pass.");
+    auto returnHome = make_shared<DialogNode>("You return home, safe but filled with regret.");
+
+    // Add more options to the runAway node
+    runAway->AddOption("Hide in a cave", hideInCave);
+    runAway->AddOption("Return home", returnHome);
 
     // Create the dialog tree
     DialogTree dialogTree(root);
@@ -20,5 +37,4 @@ int main()
     // Start the dialog
     dialogTree.StartDialog();
 
-    return 0;
 }
